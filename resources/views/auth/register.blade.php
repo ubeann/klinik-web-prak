a73fef7cede7bb50f3e99362a94078766a1b5878
ubeann <76271600+ubeann@users.noreply.github.com>
2023-06-08 15:33:18 +0700

    FIX favicon url

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
-  <link rel="icon" type="image/png" href="{{ asset('img/favicon.svg') }}">
+  <link rel="icon" type="image/png" href="{{ asset('favicon.svg') }}">
   <title>
     {{ Str::ucfirst($type ?? 'Patient') }} Sign Up
   </title>
